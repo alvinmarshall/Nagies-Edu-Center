63f3d4e656a19f1503f04c5ab273934d739de7b6
bik_krlvn <kelvinbirikorang@gmail.com>
2020-05-13 23:10:42 +0000

    update config

--- .github/scripts/decrypt_google_service.sh
+++ .github/scripts/decrypt_google_service.sh
@@ -3,4 +3,4 @@
     # Decrypt the file
     mkdir $GITHUB_WORKSPACE/secrets
     # --batch to prevent interactive command --yes to assume "yes" for questions
-    gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICE_KEY" --output $GITHUB_WORKSPACE/secrets/google-services.json google-services.json.gpg
+    gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICE_KEY" --output $GITHUB_WORKSPACE/secrets/google-services.json app/google-services.json.gpg
